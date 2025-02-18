5e5890b6cc0ff33c5ba5f9c08ab22dd1fa2e8fbf
Bhupendra sapkota <np05cp4a230235@iic.edu.np>
2025-02-18 14:35:16 +0545

    Delete frontend directory

--- frontend/src/pages/Trending/Paginated/Pagination.tsx
+++
@@ -1,9 +0,0 @@
-const Pagination = () => {
-  return (
-    <section className="flex justify-center items-center h-auto bg-black text-white">
-        hello from Pagination
-    </section>
-  )
-}
-
-export default Pagination
