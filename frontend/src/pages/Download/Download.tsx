a25e5d88a9748489967b0ea98886ddfd9a01839b
bhupendrasapkota <np05cp4a230235@iic.edu.np>
2025-02-18 14:36:54 +0545

    nothing managing files

--- frontend/src/pages/Download/Download.tsx
+++
@@ -1,7 +0,0 @@
-const Download = () => {
-  return (
-    <div className="bg-white text-black">this is my Download</div>
-  )
-}
-
-export default Download
--- frontend/src/pages/Feed/Feed.tsx
+++
@@ -1,7 +0,0 @@
-const Feed = () => {
-  return (
-    <div className="bg-white text-black">this is my Feed</div>
-  )
-}
-
-export default Feed
